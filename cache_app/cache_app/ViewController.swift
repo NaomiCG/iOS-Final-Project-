2a0048dec0979c3898d307dfc0e3e9a3fec39911
kpwill <kpw243@nyu.edu>
2018-04-29 15:09:57 -0400

    firebase auth set up for signing up

--- cache_app/cache_app/ViewController.swift
+++ cache_app/cache_app/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 
 class ViewController: UIViewController {
@@ -22,11 +23,62 @@ class ViewController: UIViewController {
     // Dispose of any resources that can be recreated.
     }
 
-
-    @IBOutlet weak var first: UITextField!
+    @IBAction func action(_ sender: Any) {
+        if email.text != "" && password.text != ""
+        {
+            let myVar = 0
+            //check if it's sign up or log in
+            if myVar == 1//log in
+            {
+                Auth.auth().signIn(withEmail: email.text!, password: password.text!, completion: { (user, error) in
+                    if user != nil
+                    {
+                        //log in successful
+                        print("Log in successful!")
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription
+                        {
+                            //we have a description of the error
+                            print(myError)
+                        }
+                        else
+                        {
+                            print("ERROR")
+                        }
+                    }
+                })
+            }
+            else //sign up
+            {
+                Auth.auth().createUser(withEmail: email.text!, password: password.text!, completion: { (user, error) in
+                    if user != nil
+                    {
+                        //sign up successful
+                        print("Sign up successful!")
+                    }
+                    else
+                    {
+                        if let myError = error?.localizedDescription
+                        {
+                            //we have a description of the error
+                            print(myError)
+                        }
+                        else
+                        {
+                            print("ERROR")
+                        }
+                    }
+                })
+            }
+        }
+    }
     
+    @IBOutlet weak var first: UITextField!
     @IBOutlet weak var last: UITextField!
-    @IBOutlet weak var email: UILabel!
-    @IBOutlet weak var password: UILabel!
-    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var birthday: UIDatePicker!
+    @IBOutlet weak var actionButton: UIButton!
 }
