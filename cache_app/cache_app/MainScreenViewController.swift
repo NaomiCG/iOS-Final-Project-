0509582cad8aa9eea06e045d3cdc7fd720f42e53
kpwill <kpw243@nyu.edu>
2018-05-06 14:46:05 -0400

    created blank VC for home screen

---
+++ cache_app/cache_app/MainScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainScreenViewController.swift
+//  cache_app
+//
+//  Created by Kelly Williams on 5/6/18.
+//  Copyright © 2018 ncgheorghe. All rights reserved.
+//
+
+import UIKit
+
+class MainScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
