ef12211bfea3e31e26cab5878148be83f164428e
Naomi Gheorghe <naomigheorghe@Naomis-MacBook-Air-3.local>
2018-05-07 08:13:33 -0400

    old commit

--- cache_app/cache_app/RegistrationInfoViewController.swift
+++ cache_app/cache_app/RegistrationInfoViewController.swift
@@ -21,9 +21,7 @@ class RegistrationInfoViewController: UIViewController, UIPickerViewDelegate, UI
     
     @IBOutlet weak var stateTextBox: UITextField!
     @IBOutlet weak var stateDropDown: UIPickerView!
-    
-    var stateList = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","MD","MA","MI","MN","MS","MO","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"
-    ]
+    var stateList = ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","MD","MA","MI","MN","MS","MO","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
     
     @IBOutlet weak var salary: UILabel!
     @IBOutlet weak var salarySlider: UISlider!
