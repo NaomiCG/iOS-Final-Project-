b7a2959940b3215ad4afa484f27583d2b21a4730
Naomi Catherine Gheorghe <ncgheorghe@Naomis-iMac.local>
2018-05-11 21:03:14 -0400

    new credit view controller

---
+++ cache_app/cache_app/LoViewController.swift
@@ -0,0 +1,35 @@
+//
+//  LoViewController.swift
+//  cache_app
+//
+//  Created by Naomi Catherine Gheorghe on 5/11/18.
+//  Copyright © 2018 ncgheorghe. All rights reserved.
+//
+
+import UIKit
+
+class LoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
